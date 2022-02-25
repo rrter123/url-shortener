6daa17592d3757eea23b910cca100da4df43c459
Alicja Gustowska <alicja.gustowska@gmail.com>
2022-02-25 17:30:13 +0100

    Add url_short app and basic models

--- backend/url_short/models.py
+++ backend/url_short/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class ShortenedLink(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    full_link = models.TextField()
+    short_link_suffix = models.TextField()
+
+
+
+    def __str__(self) -> str:
+        return f'{self.short_link_suffix}, {self.full_link}'
+
+
+class LinkUnshortening(models.Model):
+    time = models.DateTimeField(auto_now=True)
+    shortened_link = models.ForeignKey(ShortenedLink, related_name='unshortenings')
+
+    def __str__(self) -> str:
+        return f'{self.time}, {self.shortened_link}'
