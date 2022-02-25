64f7971a58b2d10948f2448aaf9bb9802b0756fb
Alicja Gustowska <alicja.gustowska@gmail.com>
2022-02-25 18:05:50 +0100

    Add basic short suffix generation

--- backend/url_short/models.py
+++ backend/url_short/models.py
@@ -1,13 +1,41 @@
 from django.db import models
 import uuid
+import random
+import string
+
+class FailedShorteningGenerationException(BaseException):
+    ...
 
 
 class ShortenedLink(models.Model):
+    DEFAULT_SUFFIX_LENGTH = 5
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_link = models.TextField()
     short_link_suffix = models.TextField()
 
+    def save(self, *args, **kwargs):
+        if not self.short_link_suffix:
+            self.short_link_suffix = self.generate_short_suffix()
+        return super().save(*args, **kwargs)
 
+    def generate_short_suffix(self) -> str:
+        # Clean this up
+        for length in range(self.DEFAULT_SUFFIX_LENGTH, self.DEFAULT_SUFFIX_LENGTH+2):
+            for _ in range(5):
+                random_string = self.generate_random_string(length)
+                if not self.string_has_collision(random_string):
+                    return random_string
+        raise FailedShorteningGenerationException
+
+    def generate_random_string(self, length: int) -> str:
+        # Move this somewhere else
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+    def string_has_collision(self, random_string: str) -> bool:
+        return ShortenedLink.objects.filter(short_link_suffix=random_string).exists()
+
+    
 
     def __str__(self) -> str:
         return f'{self.short_link_suffix}, {self.full_link}'
---
+++ backend/url_short/shortening_service.py
@@ -0,0 +1,27 @@
+import random
+import string
+from url_short.models import ShortenedLink
+
+class FailedShorteningGenerationException(BaseException):
+    ...
+
+class ShorteningService:
+    DEFAULT_SUFFIX_LENGTH = 5
+
+    @classmethod
+    def generate_short_suffix(cls) -> str:
+        for length in range(cls.DEFAULT_SUFFIX_LENGTH, cls.DEFAULT_SUFFIX_LENGTH+2):
+            for _ in range(5):
+                random_string = cls.generate_random_string(length)
+                if not cls.string_has_collision(random_string):
+                    return random_string
+        raise FailedShorteningGenerationException
+
+    @classmethod
+    def string_has_collision(cls, random_string: str) -> bool:
+        # circular import will happen here
+        return ShortenedLink.objects.filter(short_link_suffix=random_string).exists()
+
+    @classmethod
+    def generate_random_string(length: int) -> str:
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
