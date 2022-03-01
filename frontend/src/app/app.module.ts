2b624d3399bdec3c39dca67deb5fb78bf531652f
Alicja Gustowska <alicja.gustowska@gmail.com>
2022-03-01 16:46:24 +0100

    Add nebular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UrlShortenerModule } from './url-shortener/url-shortener.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
     declarations: [
@@ -14,7 +17,11 @@ import { UrlShortenerModule } from './url-shortener/url-shortener.module';
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
-        UrlShortenerModule
+        UrlShortenerModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot({ name: 'cosmic' }),
+        NbLayoutModule,
+        NbEvaIconsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
